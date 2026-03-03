3d9b35d59f8343c4f1b271f3ffe5ebdd91a99e83
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2026-03-03 14:17:49 +0530

    Added: destructuring methods

---
+++ Day2/dist/destructure_methods.js
@@ -0,0 +1,20 @@
+"use strict";
+// const foo = ["one", "two", "three"];
+// const [red, yellow, green] = foo;
+// console.log(red); 
+// console.log(yellow); 
+// console.log(green); 
+// const foo = ["one", "two"];
+// const [red, yellow, green, blue] = foo;
+// console.log(red); 
+// console.log(yellow); 
+// console.log(green); 
+// console.log(blue); 
+function f() {
+    return [1, 2, 3, 4, 5];
+}
+const arr = f();
+const [first, ...rest] = arr;
+const last = rest.pop();
+console.log(first); // 1
+console.log(last); // 5
--- Day2/dist/function.js
+++ Day2/dist/function.js
@@ -3,7 +3,7 @@ console.log(add(1, 2));
 function add(a, b) {
     return a + b;
 }
-console.log(add2(3, 4));
 const add2 = (a, b) => {
     return a + b;
 };
+console.log(add2(3, 4));
