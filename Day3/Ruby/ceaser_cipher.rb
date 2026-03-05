b7dcfb2c105dfb27b9af5d080914d537f5223c4e
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2026-03-05 15:41:16 +0530

    Day3: Added ceaser_cipher function in ruby to revice basic ruby concepts

---
+++ Day3/Ruby/ceaser_cipher.rb
@@ -0,0 +1,17 @@
+def ceaser_cipher(string, shift)
+  ciphered_string = ""
+  string = string.downcase
+
+  string.each_char do |char|
+    if char.ord.between?(97, 122)
+      shifted_char = ((char.ord - 97 + shift) % 26) + 97
+      ciphered_string += shifted_char.chr
+    else
+      ciphered_string += char
+    end
+  end
+  ciphered_string
+end  
+
+puts ceaser_cipher("Hello, World!", 3) # "khoor, zruog!"
+puts ceaser_cipher("Ruby is fun!", 5) # "wzgd nx k
