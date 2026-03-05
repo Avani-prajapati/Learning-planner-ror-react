04817e9d18f0b1599954577f73d3dc052d0a9629
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2026-03-05 14:47:59 +0530

    Day3: Added props logic and minimal design

--- Day3/component-props/src/components/Card.tsx
+++ Day3/component-props/src/components/Card.tsx
@@ -1,15 +1,16 @@
-export default function Card() {
+export default function Card({name, description, image}: {name: string, description: string, image: string}) {
   return (
     <div className="bg-white shadow-lg border-2 border-gray-400 rounded-xl p-4 w-64 hover:shadow-xl transition duration-300">
       <img
-        src="https://picsum.photos/200/300"
-        alt="Card Image"
+        src={image}
+        alt={name}
+
         className="w-full h-40 object-cover rounded-md mb-3"
       />
 
-      <h1 className="text-lg font-semibold text-gray-800 mb-1">Card Title</h1>
+      <h1 className="text-lg font-semibold text-gray-800 mb-1">{name}</h1>
 
-      <p className="text-gray-600 text-sm">This is a card component.</p>
+      <p className="text-gray-600 text-sm">{description}</p>
     </div>
   );
 }
--- Day3/component-props/src/components/Dashboard.tsx
+++ Day3/component-props/src/components/Dashboard.tsx
@@ -1,10 +1,62 @@
 import Card from "./Card";
 
 export default function Dashboard() {
+  const places = [
+    {
+      name: "Paris",
+      description: "The city of love and lights.",
+      image: "https://picsum.photos/200/300?paris",
+    },
+    {
+      name: "Tokyo",
+      description: "A bustling metropolis blending tradition and modernity.",
+      image: "https://picsum.photos/200/300?tokyo",
+    },
+    {
+      name: "New York",
+      description: "The city that never sleeps.",
+      image: "https://picsum.photos/200/300?newyork",
+    },
+    {
+      name: "New York",
+      description: "The city that never sleeps.",
+      image: "https://picsum.photos/200/300?newyork",
+    },
+    {
+      name: "New York",
+      description: "The city that never sleeps.",
+      image: "https://picsum.photos/200/300?newyork",
+    },
+    {
+      name: "New York",
+      description: "The city that never sleeps.",
+      image: "https://picsum.photos/200/300?newyork",
+    },
+    {
+      name: "New York",
+      description: "The city that never sleeps.",
+      image: "https://picsum.photos/200/300?newyork",
+    },
+    {
+      name: "New York",
+      description: "The city that never sleeps.",
+      image: "https://picsum.photos/200/300?newyork",
+    },
+  ];
   return (
     <div className="flex-1 p-4">
-        Dashboard
-        <Card />
+      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+
+      <div className="grid grid-cols-5 gap-1 ">
+        {places.map((place, index) => (
+          <Card
+            key={index}
+            name={place.name}
+            description={place.description}
+            image={place.image}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
