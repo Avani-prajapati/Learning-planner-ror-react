5cf2a795e60ca5dc2a5686ebbeadfa7744966581
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2026-03-06 14:52:44 +0530

    Day4: Added validation in sweet shop app

--- Day4/state-hooks/src/components/AddSweet.tsx
+++ Day4/state-hooks/src/components/AddSweet.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
-export default function AddSweet({ setSweets, setIsFormOpen }: { setSweets: any; setIsFormOpen: any }) {
+export default function AddSweet({
+  setSweets,
+  setIsFormOpen,
+}: {
+  setSweets: any;
+  setIsFormOpen: any;
+}) {
   const [newSweet, setNewSweet] = useState({
     name: "",
     price: 0,
@@ -9,6 +15,16 @@ export default function AddSweet({ setSweets, setIsFormOpen }: { setSweets: any;
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
+    if(!newSweet.name || !newSweet.price || !newSweet.description) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    if(newSweet.price <= 0) {
+      alert("Price must be greater than 0");
+      return;
+    }
+
     setSweets((prev: any) => [...prev, { ...newSweet, id: Date.now() }]);
     setNewSweet({
       name: "",
@@ -18,46 +34,56 @@ export default function AddSweet({ setSweets, setIsFormOpen }: { setSweets: any;
     setIsFormOpen(false);
   }
 
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    
+    setNewSweet({
+      ...newSweet,
+      [name]: value,
+    });
+  }
+
   return (
     <form
       onSubmit={handleSubmit}
       className="bg-white border rounded-lg shadow-sm p-6 max-w-md mx-auto space-y-4"
     >
       <h2 className="text-xl font-semibold text-gray-800">Add New Sweet</h2>
-      <label className="block text-sm font-medium text-gray-700">Name: 
-
-      <input
-        type="text"
-        placeholder="Sweet Name"
-        value={newSweet.name}
-        onChange={(e) => setNewSweet({ ...newSweet, name: e.target.value })}
-        className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-400"
+      <label className="block text-sm font-medium text-gray-700">
+        Name:
+        <input
+          type="text"
+          name="name"
+          placeholder="Sweet Name"
+          value={newSweet.name}
+          onChange={handleChange}
+          required
+          className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-400"
         />
       </label>
 
-
-      <label className="block text-sm font-medium text-gray-700">Price:
-      <input
-        type="number"
-        placeholder="Price"
-        value={newSweet.price}
-        onChange={(e) =>
-          setNewSweet({ ...newSweet, price: parseFloat(e.target.value) || 0 })
-        }
-        className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-400"
-      />
+      <label className="block text-sm font-medium text-gray-700">
+        Price:
+        <input
+          type="number"
+            name="price"
+          placeholder="Price"
+          value={newSweet.price}
+          onChange={handleChange}
+          className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-400"
+        />
       </label>
 
-      <label className="block text-sm font-medium text-gray-700">Description:
-      <input
-        type="text"
-        placeholder="Description"
-        value={newSweet.description}
-        onChange={(e) =>
-          setNewSweet({ ...newSweet, description: e.target.value })
-        }
-        className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-400"
-      />
+      <label className="block text-sm font-medium text-gray-700">
+        Description:
+        <input
+          type="text"
+            name="description"
+          placeholder="Description"
+          value={newSweet.description}
+          onChange={handleChange}
+          className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-400"
+        />
       </label>
 
       <div className="flex gap-3">
