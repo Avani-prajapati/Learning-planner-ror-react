e7d79ae0c10dae00addf085c95a309f9fa64e4dd
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2026-03-10 16:34:41 +0530

    Day7: Added crud api controller logic for post also tested with postman

--- Day7/blog_api/app/controllers/posts_controller.rb
+++ Day7/blog_api/app/controllers/posts_controller.rb
@@ -1,11 +1,46 @@
 class PostsController < ApplicationController
-   def index
+
+  def index
     posts = Post.all
     render json: posts
   end
 
   def show
-  post = Post.find(params[:id])
-  render json: post
+    post = Post.find(params[:id])
+    render json: post
   end
+
+  def create
+    post = Post.new(post_params)
+
+    if post.save
+      render json: post, status: :created
+    else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+def update
+  post = Post.find(params[:id])
+
+  if post.update(post_params)
+    render json: post
+  else
+    render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+  end
+end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+
+    render json: { message: "Post deleted successfully" }
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :content)
+  end
+
 end
