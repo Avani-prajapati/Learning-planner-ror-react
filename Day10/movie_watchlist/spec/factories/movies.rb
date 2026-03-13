b567d7ad426c493f1c6bd7f8820056f49896d270
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2026-03-13 16:39:47 +0530

    Day10: Added one factory file to add dummy movie data (DRY)

--- Day10/movie_watchlist/spec/factories/movies.rb
+++ Day10/movie_watchlist/spec/factories/movies.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :movie do
-    title { "MyString" }
-    year { "MyString" }
-    genre { "MyString" }
-    plot { "MyText" }
-    poster_url { "MyString" }
-    imdb_id { "MyString" }
-    watched { false }
-    rating { "9.99" }
+   title      { Faker::Movie.title }
+    year       { rand(1990..2024).to_s }
+    genre      { %w[Action Drama Comedy Thriller].sample }
+    plot       { Faker::Lorem.paragraph }
+    poster_url { Faker::Internet.url }
+    imdb_id    { "tt#{rand(1000000..9999999)}" }
+    watched    { false }
+    rating     { rand(1.0..10.0).round(1) }
   end
 end
