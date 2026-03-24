84239028ce49dd6f8c8e42e77eee6edaf5374341
Avani-prajapati <avani@incubyte.co>
2026-03-24 17:08:31 +0530

    feat: setup apollo client and test graphql connection to rails backend

--- Day8/frontend/src/App.tsx
+++ Day8/frontend/src/App.tsx
@@ -1,18 +1,54 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client/react';
+import { Box, Heading, Text, Spinner } from '@chakra-ui/react';
+
+interface Post {
+  id: string,
+  title: string
+}
+
+interface GetPostsQuery {
+  posts : Post[];
+}
+
+const TEST_QUERY = gql`
+  query {
+    posts {
+      id
+      title
+    }
+  }
+`;
 
 function App() {
+  const { data, loading, error } = useQuery<GetPostsQuery>(TEST_QUERY);
+
+  console.log(data)
   return (
     <Box className="min-h-screen bg-gray-100 flex items-center justify-center">
       <Box textAlign="center" p={8}>
-        <Heading size="xl" mb={2}>
-          🚀 Setup working!
+        <Heading size="xl" mb={4}>
+          Apollo Connection Test
         </Heading>
-        <Text color="gray.500" mb={4}>
-          Chakra UI v3 is loaded.
-        </Text>
-        <p className="text-blue-500 font-bold text-lg">
-          This blue text confirms Tailwind v4 works.
-        </p>
+
+        {loading && <Spinner size="lg" />}
+
+        {error && (
+          <Text color="red.500">
+            ❌ Error: {error.message}
+          </Text>
+        )}
+
+        {data && (
+          <Box>
+            <Text color="green.500" fontWeight="bold" mb={2}>
+              ✅ Connected to Rails GraphQL!
+            </Text>
+            <Text color="gray.600">
+              Found {data.posts.length} posts
+            </Text>
+          </Box>
+        )}
       </Box>
     </Box>
   );
--- Day8/frontend/src/main.tsx
+++ Day8/frontend/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { ApolloProvider } from '@apollo/client/react'
+import client from './apolloClient.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
-    <App />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider value={defaultSystem}>
+        <App />
+      </ChakraProvider>
+    </ApolloProvider>
   </StrictMode>,
 )
