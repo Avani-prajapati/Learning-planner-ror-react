c32312365d0554255a87b538239072803e421be6
Avani-prajapati <avani@incubyte.co>
2026-03-25 14:26:33 +0530

    Added article type

---
+++ blog_api/app/graphql/types/article_type.rb
@@ -0,0 +1,14 @@
+module Types
+  class ArticleType < Types::BaseObject
+    description "A blog article"
+
+    field :id,         ID,     null: false
+    field :title,      String, null: false
+    field :body,       String, null: false
+    field :status,     String, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :user,       Types::UserType,      null: false
+    field :comments,   [Types::CommentType], null: false
+    field :tags,       [Types::TagType],     null: false
+  end
+end
